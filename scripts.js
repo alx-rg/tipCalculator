fe5b1b24479d475ae9d439e499dff624d9dfe212
alx-rg <90330429+alx-rg@users.noreply.github.com>
2021-10-05 21:56:29 -0500

    JS complete

--- scripts.js
+++ scripts.js
@@ -1,85 +1,63 @@
+import { randomColor, randomFont } from './helpers.js'
 
-window.onload = () =>
-//the function called when Calculate button is clicked.
-{
-//*calling a function calculateTip
-// which will calculate the tip for the bill.*/
-	 document.querySelector('#calculate').onclick = calculateTip;
+// Get a reference to the body tag
+const body = document.querySelector('body')
+
+// Add a single event listener listening for click events
+body.addEventListener('click', onClick)
+
+
+// This function handles a click when it occurs on the body
+function onClick(e) {
+	// The event object e is important in this example. 
+	// It has a property e.target which tells us exactly which 
+	// element was clicked. 
+	// TODO: Open the test page, click around the page and look at the console.
+	console.log(e.target)
+
+	// Below we will check the element and decide what to do about the click.
+	// For each click we can use .matches() with a CSS selector to 
+	// "match" that element. You can match any CSS selector! 
+	// Notice below I used: name, id, class
+	if (e.target.matches('h1')) {
+		// You clicked the h1
+		e.target.style.color = randomColor()
+
+	} else if (e.target.matches('p')) {
+		e.target.style.fontFamily = randomFont()
+
+	} else if (e.target.matches('#make-box')) {
+		// When we click the make box button
+		const box = document.createElement('div') // Make a new element
+		box.classList.add('box')
+		box.style.backgroundColor = randomColor()
+		const boxContainer = document.querySelector('.box-container')
+		boxContainer.appendChild(box)
+
+	}
 }
 
-//let defaultBill = 0.00;
-//let defaultTip = 0.00;
-//let defaultPerson = 0;
-
-
-const inputBill = document.querySelector('#input-bill');
-const inputTip = document.querySelector('#input-tip');
-const inputPeople = document.querySelector('#input-people');
-
-inputBill.addEventListener('input-bill', calculateTip)
-inputBill.addEventListener('input-tip', calculateTip)
-inputBill.addEventListener('input-people', calculateTip)
-
-function updateCalc (e) {
-	let target = e.target.id;
-	console.log(target);
-
-//	if (target.tagName === );
-
-}
-
-function calculateTip() {
-	//INPUTS
-	const inputBill = Number(document.querySelector('#input-bill').value);
-	const inputTip = Number(document.querySelector('#input-tip').value);
-	const inputPeople = Number(document.querySelector('#input-people').value);
-	//OUTPUTS 
-	let outputTip = Number(document.querySelector('#output-tip-total').value);
-	let outputBillTotal = Number(document.querySelector('#output-bill-total').value);
-	let outputTipEachPerson = Number(document.querySelector('#output-tip-each-person').value);
-	let outputBillEachPerson = Number(document.querySelector('#output-bill-each-person-total').value);
-
-
-	section1.addEventListener('change', calculateTip);
-
-
-	//let target = e.target.id;
-	//console.log(target)
-		
-	console.log(inputBill)
-	console.log(inputTip)
-	console.log(inputPeople)
-
-	//Output total of the tip based on the percentage given
-	let tipTotal = (inputTip / inputBill) * 100;
-	outputTip = Number(tipTotal.toFixed(2));
-	
-	//console.log(outputTip);
-
-
-	//Output total of the Bill including the tip
-	let billTotal = inputBill + tipTotal;
-	outputBillTotal = Number(billTotal.toFixed(2));
-	
-	//console.log(outputBillTotal);
-
-	//Output the TIP amount for each person in the group
-	let tipTotalEachP = outputTip / inputPeople;
-	outputTipEachPerson = Number(tipTotalEachP.toFixed(2));
-		
-	//console.log(outputTipEachPerson);
-
-
-
-	//Output the TOTAL BILL including tip for each person
-	let billPlusTipPerPerson = outputBillTotal / inputPeople;
-	outputBillEachPerson = Number(billPlusTipPerPerson.toFixed(2));
-	
-	//console.log(outputBillEachPerson);
-
-
-	document.querySelector('#output-tip-total').innerHTML = outputTip;
-	document.querySelector('#output-bill-total').innerHTML = outputBillTotal;
-	document.querySelector('#output-tip-each-person').innerHTML = outputTipEachPerson;
-	document.querySelector('#output-bill-each-person-total').innerHTML = outputBillEachPerson;
-}
+// Using the code above try these challenges: 
+// 1. Add a new element to HTML in index.html
+// Then come back to this file and handle clicks 
+// on that element.
+// 
+// 2. Create a couple input tags in index.html. 
+// Think of a selector that will identify these elements. 
+// Add an event listener to the body that listens for 
+// 'input' events. Handle the input events from the 
+// body by using e.target.matches() to identify 
+// your inputs. 
+// 
+// 3. Apply event delegation to your Tip calculator. 
+// For this you are already using using it to some 
+// degree since you probably have a single event 
+// handler handling events from all of your inputs. 
+// Try moving this listener to the body element 
+// and checking the event target to see if the the
+// target matches the type `input'. Something like 
+// this: e.target.matches('input')
+// 
+// 4. The waypoints example uses event delgation 
+// Look at that and implement the waypoints 
+// navigation bar in your single page site. 
